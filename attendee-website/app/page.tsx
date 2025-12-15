3df6656a6481f43cea678b50508cb37d7bdc0003
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2025-12-15 21:43:28 +0700

    feat: moved dashboard route

--- attendee-website/app/page.tsx
+++ attendee-website/app/page.tsx
@@ -6,25 +6,5 @@ import CalendarCard from "@/components/dashboard/CalendarCard";
 import InsightsSection from "@/components/dashboard/InsightsSection";
 
 export default function Home() {
-  return (
-    <>
-      <ResponsiveAppBar />
-
-      <main className="p-6 w-full mx-auto flex flex-col md:flex-row gap-6">
-        <div className="flex flex-col-reverse md:flex-col flex-2 gap-6 md:flex-1">
-          <div>
-            <div className="pb-3 font-semibold">Insights</div>
-            <InsightsSection />
-          </div>
-          <div>
-            <div className="pb-3 font-semibold">Attendance</div>
-            <div className="flex flex-col-reverse md:flex-row gap-6">
-              <CalendarCard />
-              <ClockCard />
-            </div>
-          </div>
-        </div>
-      </main>
-    </>
-  );
+  return <>this will be a router</>;
 }
