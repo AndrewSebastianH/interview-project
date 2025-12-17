47dfa46607aceb487ccf505eae531f4086151f72
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2025-12-17 14:07:17 +0700

    feat: multer config and setup

--- attendee-backend/src/main.ts
+++ attendee-backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
 
   const configService = app.get(ConfigService);
 
